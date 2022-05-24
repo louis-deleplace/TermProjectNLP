50145b7a17b7f7f583cde3e56d85f3543c6df80e
Tony Zhou <tony.zhou@epitech.eu>
2022-05-24 19:53:18 +0200

    feat(back): add tf idf + cos similarity and route getSong

--- app/app.py
+++ app/app.py
@@ -9,16 +9,23 @@ import xml.etree.ElementTree as ET
 from flask_mysqldb import MySQL
 from flask import jsonify
 from flask_cors import CORS
+from flask import request
+import numpy
+import pandas
+import sys
+import re
+
 
 app = Flask(__name__)
-CORS(app)
 
 app.config['MYSQL_HOST'] = 'db'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'songs'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+CORS(app, resources={r"/*": {"origins": "*"}})
 mysql = MySQL(app)
 
 
@@ -133,6 +140,88 @@ def getAll():
     cursor.close()
 
     return jsonify(data)
+
+
+@app.route('/song', methods=["GET"])
+def getSong():
+    artist = request.args.get("artist")
+    title = request.args.get("title")
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM songs where artist = " +
+                   artist + "AND title = " + title + " LIMIT 1")
+    data = cursor.fetchall()
+    cursor.close()
+
+    return jsonify(data)
+
+
+@app.route('/research', methods=["POST"])
+def research():
+    # request.form
+    cursor = mysql.connection.cursor()
+    # data = cursor.fetchall()
+    data = pandas.read_sql("SELECT * FROM songs", mysql.connection)
+    args = request.get_json()
+    print(request, file=sys.stderr)
+    print(args, file=sys.stderr)
+    UI_BPM = args["UI_BPM"]
+    UI_DANCE = args["UI_DANCE"]
+    UI_ENERGY = args["UI_ENERGY"]
+    lyrics = args["lyrics"]
+    COEF_BPM = 2
+    COEF_ENERGY = 1
+    COEF_DANCE = 1
+    # print(UI_BPM, file=sys.stderr)
+    data['filter'] = data.apply(lambda x: (COEF_BPM * abs((x['bpm'] - UI_BPM) / UI_BPM) + COEF_ENERGY * abs((x['energy'] - UI_ENERGY) /
+                                UI_ENERGY) + COEF_DANCE * abs((x['danceability'] - UI_DANCE) / UI_DANCE)) / (COEF_BPM + COEF_DANCE + COEF_ENERGY), axis=1)
+    data = data.sort_values('filter').iloc[0:50]
+    data = data.loc[data['lyrics'].str.len() > 0]
+    vocabulary = data.copy()['lyrics']
+
+    def process(lyrics):
+        if lyrics is None:
+            return ['']
+        lyrics = lyrics.lower()
+        lyrics = ' '.join(lyrics.split('\n'))
+        lyrics = re.split('[,.?!:;() \'\"*&-1234567890\[\]\`=_{}]', lyrics)
+        lyrics = [x for x in lyrics if x != '']
+        return lyrics
+
+    data['lyrics'] = data.apply(lambda x: process(x['lyrics']), axis=1)
+    data = data.drop('filter', 1).reset_index(drop=True)
+    data = data.append({'lyrics': process(lyrics)}, ignore_index=True)
+    vocabulary = sorted(list(set(process(vocabulary.str.cat(sep=' ')))))
+    N = len(data['lyrics'])
+    vectors = numpy.zeros((N, len(vocabulary)))
+
+    for w in range(len(vocabulary)):
+        word = vocabulary[w]
+        tf = numpy.array([])
+        idf = 0
+        for text in data['lyrics']:
+            f = 0
+            if word in text:
+                idf += 1
+            for t in text:
+                if word == t:
+                    f += 1
+            tf = numpy.append(tf, f / len(text))
+        if idf == 0:
+            print(word)
+        vectors[:, w] = tf * numpy.log(N / idf)
+    vectors = pandas.DataFrame(vectors, columns=vocabulary)
+
+    def cosine(x, y):
+        try:
+            return numpy.divide(numpy.dot(x, y), numpy.multiply(numpy.sqrt(numpy.dot(x, x)), numpy.sqrt(numpy.dot(y, y))))
+        except Exception:
+            return 0
+
+    data['similarity'] = vectors.apply(lambda x: round(100 * cosine(
+        numpy.array(x), vectors.iloc[-1, :]), 2), axis=1)
+    data = data.iloc[:-1].sort_values('similarity',
+                                      ascending=False)
+    return data[['title', 'artist', 'bpm', 'energy', 'danceability', 'similarity']].to_json(orient='records')
 
 
 if __name__ == '__main__':
