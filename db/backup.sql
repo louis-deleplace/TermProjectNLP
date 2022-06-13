ba3128d3c206b9b50178019d647f5ad3bc473690
Tony Zhou <tony.zhou@epitech.eu>
2022-06-13 05:14:57 +0200

    fix: when we start the docker-compose, it will take the backup.sql as a init script

--- db/backup.sql
+++ db/backup.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `songs`
 --
 
+CREATE DATABASE IF NOT EXISTS songs;
+USE songs;
+
 DROP TABLE IF EXISTS `songs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
